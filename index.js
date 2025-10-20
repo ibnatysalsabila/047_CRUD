5699e99844bcb4da963768c42a0ebe8f6f74b8d1
ibnatysalsabila <salsajpr01@gmail.com>
2025-10-20 12:31:58 +0700

    menambahkan route utama

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
