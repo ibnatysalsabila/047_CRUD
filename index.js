c96d8c2865dcb955a0d5c4fe33f069d0d70cfcda
ibnatysalsabila <salsajpr01@gmail.com>
2025-10-20 12:34:55 +0700

    menambahkan route untuk mendapatkan semua data user

--- index.js
+++ index.js
@@ -28,3 +28,13 @@ db.connect((err) => {
     console.log('Connected succesfully');
 })
 
+app.get('api/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            console.error('Error fetching users:', err);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        res.json(results);
+    })
+})
